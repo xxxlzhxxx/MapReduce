95927aee110c23cee0c168bbd7a893d4b9156658
xxxlzhxxx <zihengsjtu@gmail.com>
2023-03-17 20:32:40 -0400

    tests

---
+++ tests/testdata/exec/wc_map.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Word count mapper."""
+import sys
+
+
+
+
+for line in sys.stdin:
+    words = line.split()
+    assert False, "FIXME: for each word, print the word and a '1'"
