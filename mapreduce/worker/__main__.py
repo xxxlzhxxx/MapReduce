6890c440aec3a6ba8b55c9bf408e5890586941d9
xttt <xttt@umich.edu>
2023-03-21 02:42:18 -0400

    mapping worker pass

--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -4,10 +4,14 @@ import logging
 import json
 import time
 import click
+import tempfile
 import mapreduce.utils
 import socket
 import collections
+import shutil
 import threading
+import hashlib
+import subprocess
 
 # Configure logging
 LOGGER = logging.getLogger(__name__)
@@ -115,6 +119,8 @@ class Worker:
                     if message_dict['message_type'] == 'register_ack':
                         self.start = True
                         
+                    elif message_dict['message_type'] == 'new_map_task':
+                        self.handle_mapping(message_dict)
 
                     # receive shutdown message, send shut down message to every worker
                     elif message_dict['message_type'] == 'shutdown':
@@ -134,6 +140,55 @@ class Worker:
                 'worker_port': self.port
             }
             sock.sendall(json.dumps(message).encode('utf-8'))
+    
+    def handle_mapping(self, message_dict):
+        while not self.shutdown:
+            executable = message_dict['executable']
+            prefix = f"mapreduce-local-task{message_dict['task_id']}-"
+            with tempfile.TemporaryDirectory(prefix=prefix) as tmpdir:
+                for input_path in message_dict['input_paths']:
+                    LOGGER.info("Created %s", tmpdir)
+                    with open(input_path) as infile:
+                        with subprocess.Popen(
+                            [executable],
+                            stdin=infile,
+                            stdout=subprocess.PIPE,
+                            text=True,
+                        ) as map_process:
+                            LOGGER.info("Executed %s input=%s", executable, input_path)
+                            for line in map_process.stdout:
+                                print(line)
+                                key, value = line.split('\t')
+                                hexdigest = hashlib.md5(key.encode("utf-8")).hexdigest()
+                                keyhash = int(hexdigest, base=16)
+                                partition_number = keyhash % message_dict['num_partitions']
+                                intermediate_file = os.path.join(tmpdir, f"maptask{message_dict['task_id']:05}-part{partition_number:05}")
+                                with open(intermediate_file, 'a') as f:
+                                    f.write(line)
+                for file in os.listdir(tmpdir):
+                    with open(os.path.join(tmpdir, file), "r") as f:
+                        sorted_line = sorted(f.readlines())
+                    with open(os.path.join(tmpdir, file), "w") as f:
+                        f.writelines(sorted_line)
+                    LOGGER.info("Sorted %s", os.path.join(tmpdir, file))
+                for file in os.listdir(tmpdir):
+                    old_path = os.path.join(tmpdir, file)
+                    output_path = os.path.join(message_dict['output_directory'], file)
+                    shutil.move(old_path, output_path)
+                    LOGGER.info("Moved %s -> %s", old_path, output_path)
+            LOGGER.info("Removed %s", tmpdir)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((self.manager_host, self.manager_port))
+                message = {
+                    'message_type': 'finished',
+                    'task_id': message_dict['task_id'],
+                    'worker_host': self.host,
+                    'worker_port': self.port
+                }
+                sock.sendall(json.dumps(message).encode('utf-8'))
+            break
+
+
 
 
 @click.command()
